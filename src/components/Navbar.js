d58b7fa677e4416f081b696828bdcbbf831d03ce
Corey McCue <coreysmccue@gmail.com>
2021-05-09 09:01:09 +0200

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'gatsby'
 import { FiAlignJustify } from 'react-icons/fi'
 import logo from '../assets/images/logo.png'
 
 const Navbar = () => {
+    const [show, setShow] = useState(false)
     return (
         <nav className="navbar">
             <div className="nav-center">
@@ -11,11 +12,11 @@ const Navbar = () => {
                     <Link to="/">
                         <img src={logo} alt="Home Cookin Logo" />
                     </Link>
-                    <button className="nav-btn">
+                    <button className="nav-btn" onClick={()=> setShow()}>
                         <FiAlignJustify />
                     </button>
                 </div>
-                <div className="nav-links show-links">
+                <div className={show ? "nav-links show-links" : "nav-links"}>
                     <Link to="/" className="nav-link" activeClassName="active-link">home</Link>
                     <Link to="/recipes" className="nav-link" activeClassName="active-link">recipes</Link>
                     <Link to="/tags" className="nav-link" activeClassName="active-link">tags</Link>
