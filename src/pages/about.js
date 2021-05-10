4bcf96e5ab39b751f21b1887683946afd66e6aa9
Corey McCue <coreysmccue@gmail.com>
2021-05-10 11:44:19 +0200

    finish styling about page and start on home page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 
-const about = () => {
+
+const About = () => {
     return (
         <Layout>
-            <h2>
-                hello from about
-            </h2>
+            <main className="page">
+                <section className="about-page">
+                    <article>
+                        <h2>Our story is one of passion for food</h2>
+                        <p>I'm baby tousled pug meh try-hard occupy readymade affogato mlkshk swag truffaut thundercats schlitz blog keytar jean shorts. Knausgaard pug cray unicorn, semiotics biodiesel vexillologist poutine everyday carry next level vegan.</p>
+                        <p>Brooklyn vape actually, cornhole umami blue bottle meggings waistcoat lo-fi keytar jianbing swag banjo godard. Wayfarers waistcoat you probably haven't heard of them swag stumptown slow-carb authentic messenger bag cardigan knausgaard asymmetrical raw denim pork belly.</p>
+                        {/* <Link to="/contact" className="btn">
+                            contact
+                        </Link> */}
+                    </article>
+                    <StaticImage
+                        src="../assets/images/chef.jpeg"
+                        alt="fire chef cooking"
+                        className="about-img"
+                        placeholder="blurred"/>
+                </section>
+            </main>
         </Layout>
     )
 }
 
-export default about
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,26 @@
-import React from "react"
-import { Link } from 'gatsby'
+import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/Layout'
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/chop.jpeg"
+            alt="chopping cilantro"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth" />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Home Cookin</h1>
+              <h4>For those who care about what they eat</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   )
 }
